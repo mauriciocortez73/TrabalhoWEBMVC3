a3f76de20b414b3119149ffb6464308d002b2cde
Mauricio <maumau123br.cortez@gmial.com>
2023-11-11 15:14:22 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TrabalhoWEBMVC3.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Empresa> Empresas { get; set; }
+    }
+}
---
+++ Models/Empresa.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrabalhoWEBMVC3.Models
+{
+    public class Empresa
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Campo Descricao é obrigatório...")]
+        [StringLength(35)]
+        [Display(Name = "Descricao")]
+        public string descricao { get; set; }
+        //
+        [Required(ErrorMessage = "Campo CNPJ é obrigatório...")]
+        [StringLength(18)]
+        [Display(Name = "CNPJ")]
+        public string cnpj { get; set; }
+    }
+}
